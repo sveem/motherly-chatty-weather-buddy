248e7860080c982def61d15419ca57980ecb6bd3
DOH1211 <dohun.p@gmail.com>
2016-08-26 15:57:55 -0400

    added service, getting geo location info from window

---
+++ app/client/services/service.js
@@ -0,0 +1,23 @@
+angular.module('chattyWeather.service', [])
+
+.factory('goGet', function($window, $http) {
+  
+  var getWeatherData = function () {
+    return new Promise(function(resolve, reject) {
+      $window.navigator.geolocation.getCurrentPosition(function(location) {
+        var latitude = Math.floor(location.coords.latitude);
+        var longitude = Math.floor(location.coords.longitude);
+        console.log('here', latitude, longitude);
+        resolve($http({
+          method: 'GET',
+          url: '/api/weather',
+          params: {latitude: latitude, longitude: longitude}
+        }));
+      });    
+    });
+  };
+
+  return {
+    getWeatherData: getWeatherData
+  };
+})
