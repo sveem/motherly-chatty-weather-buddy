45d18fb920eb50eb67a25d225344161677e4ce07
wentran <wen.tran1@gmail.com>
2016-08-25 20:22:44 -0400

    weatherAPI in progress

--- app/web/weather/weatherController.js
+++ app/web/weather/weatherController.js
@@ -1,42 +1,58 @@
-// api call by zipcode
-
 var router = require("express").Router();
     Q       = require('q');
-    var mongoose = require('mongoose');
-
+var mongoose = require('mongoose');
 var unirest = require('unirest');
+var cors = require("cors");
 
-// var xhr = new XMLHttpRequest();
-// var getLocation = xhr.navigator.geolocation.getCurrentPosition(function(position) {
-//  return(position.coords.latitude, position.coords.longitude);
-// })
 
+// var endPoints ="http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&APPID=bff0f682235785a793d3f9aade60fc80"
 
 console.log("hereeee")
-// getLocation = longitude and lat from client
-// var Weather = require('../models/chatty');
-var endPoints ="http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&APPID=bff0f682235785a793d3f9aade60fc80"
+
+
+
+
+
+function getEndPoint(latNum, lonNum){
+var endPoint = "http://api.openweathermap.org/data/2.5/weather?lat=" + latNum + "&lon=" + lonNum + "&APPID=bff0f682235785a793d3f9aade60fc80"
+return endPoint;
+}
+
+
 
 // module.exports = {
 
-// create : function (req, res) {
-//   var weatherStatus = new WeatherStatus(req.body);
-//   weatherStatus.save(function (err, result) {
-//     res.json(result);
-//   });
-// },
 
-// getDisplay: function (req, res) {
+
+  console.log("ding ding ding")
+unirest.get("http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&APPID=bff0f682235785a793d3f9aade60fc80")
+.header("Accept", "application/json")
+.end(function (result) {
+  console.log(result.body)
+  // res.send(result.body)
+})
+
+
+
+
+
+// createStatus : function (req, res) {
+
+//     // var weatherStatus = new WeatherStatus(req.body);
+//   // weatherStatus.save(function (err, result) {
+//   //   res.json(result);
+//   // });
+// }
+
+// displayStatus: function (req, res) {
 //   WeatherStatus.find({}, function (err, results) {
 //     res.json(results);
 //   });
 // }
 
-unirest.get(endPoints)
-.header("Accept", "application/json")
-.end(function (result) {
-  console.log(result.body)
-  // res.send(result)
-});
+
 // }
-// module.exports = router;
+
+
+
+module.exports = router;
