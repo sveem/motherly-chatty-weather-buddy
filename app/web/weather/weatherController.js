dff3b9bf576c0adc50c46e3056aafb931def5b3d
wentran <wen.tran1@gmail.com>
2016-08-25 21:06:11 -0400

    weatherApi in the works

--- app/web/weather/weatherController.js
+++ app/web/weather/weatherController.js
@@ -7,9 +7,6 @@ var cors = require("cors");
 
 // var endPoints ="http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&APPID=bff0f682235785a793d3f9aade60fc80"
 
-console.log("hereeee")
-
-
 
 
 
@@ -18,22 +15,24 @@ var endPoint = "http://api.openweathermap.org/data/2.5/weather?lat=" + latNum + 
 return endPoint;
 }
 
-unirest.get("http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&APPID=bff0f682235785a793d3f9aade60fc80")
-.header("Accept", "application/json")
-.end(function (result) {
-  console.log(result.body)
+unirest.get(getEndPoint(35,139))
+.headers("Accept", "application/json")
+.end(function (response) {
+console.log(response.body)
 })
 
 module.exports = {
 
+
 createStatus : function (req, res) {
-  // res.send(result.body)
+  console.log(data)
+  //   var weatherStatus = new WeatherStatus(req.body);
+  // weatherStatus.save(function (err, result) {
+  //   res.json(result);
+  // });
 }
-//     // var weatherStatus = new WeatherStatus(req.body);
-//   // weatherStatus.save(function (err, result) {
-//   //   res.json(result);
-//   // });
-// }
+
+}
 
 // displayStatus: function (req, res) {
 //   WeatherStatus.find({}, function (err, results) {
@@ -42,8 +41,8 @@ createStatus : function (req, res) {
 // }
 
 
-}
+// }
 
-}
+
 
 // module.exports = router;
