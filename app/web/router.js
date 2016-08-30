c48296c450f747dc6b0e8f0bb9129b24c032f33c
Svetlin Mladenov <svem@gmx.com>
2016-08-30 13:38:24 -0400

    router

--- app/web/router.js
+++ app/web/router.js
@@ -1,14 +1,14 @@
 var router = require('express').Router();
 var weatherController = require('./weather/weatherController.js');
 var phraseController = require('./phrases/phraseController.js');
+var transportationController = require('./transportation/transportationController.js');
 
   router.get('/api/weather/', weatherController.getWeather);
+  router.get('/api/transportation/', transportationController.getTransportEvents);
   // router.get('/api/phrases/', phraseController.getPhrases);
   // router.post('/api/phrases/', phraseController.addPhrase);
 
   // router.post('/api/weather/', weatherController.createStatus);
-
-
 
 
 module.exports = router
