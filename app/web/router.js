6e3e726b3ca4ea38f1504f21a636ae6feb65d864
Svetlin Mladenov <svem@gmx.com>
2016-08-30 14:49:34 -0400

    some fixes

--- app/web/router.js
+++ app/web/router.js
@@ -3,7 +3,7 @@ var weatherController = require('./weather/weatherController.js');
 var phraseController = require('./phrases/phraseController.js');
 var transportationController = require('./transportation/transportationController.js');
 
-  router.get('/api/weather/', weatherController.getWeather);
+  router.get('/api/weather/', weatherController.getHourly);
   router.get('/api/transportation/', transportationController.getTransportEvents);
   router.get('/api/phrases/', phraseController.getPhrases);
   router.post('/api/phrases/', phraseController.addPhrase);
