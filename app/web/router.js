51316677e6ee21065f063e566c28c316b60e08c5
Svetlin Mladenov <svem@gmx.com>
2016-09-01 16:34:14 -0400

    subway data-fixed

--- app/web/router.js
+++ app/web/router.js
@@ -6,7 +6,7 @@ var logicController = require('./logic/weatherLogicController');
 var activityController = require('./activities/activityController');
 
 router.get('/api/weather/', weatherController.getHourly, logicController.getWeatherPhrases, serviceAlerts.mtaInfo, phraseController.getPhrases);
-// router.get('/api/service/', serviceAlerts.mtaInfo); //do we need it?
+router.get('/api/transportation/', serviceAlerts.getAllMtaData ); 
 router.get('/api/phrases/', phraseController.getPhrases);
 router.post('/api/phrases/', phraseController.addPhrase);
 
